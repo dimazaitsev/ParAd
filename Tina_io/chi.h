02e4032c1b2ec1fdfe0a3e602e4991b3c6778022
Dmitry Zaitsev <daze@acm.org>
2019-04-12 12:03:24 -0400

    Delete chi.h

--- Tina_io/chi.h
+++
@@ -1,8 +0,0 @@
-// ParAd: Parallel Adriana 
-// chi.h                                 
-// Check invariance of a Petri net on basis invariants in SPM format  
-
-int CheckInv( char * NetFileName, char * InvFileName );
-
-// @ 2018 Dmitry Zaitsev, Stanimire Tomov, and Jack Dongarra: daze@acm.org
-
