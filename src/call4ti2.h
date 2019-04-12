ddf90a203a7fb2e667c2c68db2f531921d4de472
Dmitry Zaitsev <daze@acm.org>
2019-04-12 11:55:37 -0400

    Delete call4ti2.h

--- src/call4ti2.h
+++
@@ -1,8 +0,0 @@
-// ParAd: Parallel Adriana 
-// call4ti2.h                                   
-// Solve a linear Diophantine homogeneous system running "zsolve" from 4ti2    
-
-int zsolve4ti2( char * SolverName, char * SystemFileName, char * SolutionFileName, int debug_level );
-
-// @ 2018 Dmitry Zaitsev, Stanimire Tomov, and Jack Dongarra: daze@acm.org
-
